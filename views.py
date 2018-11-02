e4496255d762975bb9ed3d7bd38fdc2529b6f136
Aditya Shastri <ad.shastri1@gmail.com>
2018-11-02 14:35:32 -0400

    templates

--- views.py
+++ views.py
@@ -1,5 +1,6 @@
-from flask import Flask, redirect, request
+from flask import Flask, redirect, request, render_template
 from pymongo import MongoClient
+import random
 
 #constants
 DEFAULT_LENGTH = 7
@@ -21,25 +22,26 @@ def index():
 		if short_url is None or "":
 			rand_url = gen_random_string()
 			coll.insert_one({"short_url": rand_url, "long_url": long_url})
-			return "success"
+			return render_template("index.html", url=rand_url)
 		#selected
 		else:
 			result = coll.find_one({"short_url": short_url})
+			print(result)
 			if result is None:
 				coll.insert_one({"short_url": short_url, "long_url": long_url})
-				return "success"
+				return render_template("index.html", url=short_url)
 			else:
-				return "error template"
+				return render_template("index.html", error="Error: Short URL already exists")
 	else:
-		return "render template"
+		return render_template("index.html", homepage="True")
 
 @app.route('/<short_url>')
 def lookup(short_url):
 	result = coll.find_one({"short_url": short_url})
 	if result is None:
-		return "should be an error template"
+		return "error template"
 	else:
-		redirect(result['long_url'])
+		return redirect(result['long_url'])
 
 def gen_random_string(len=DEFAULT_LENGTH, allowed_chars=ALLOWED_CHARS):
 	rand = ''.join(random.choice(allowed_chars) for i in range(len))
